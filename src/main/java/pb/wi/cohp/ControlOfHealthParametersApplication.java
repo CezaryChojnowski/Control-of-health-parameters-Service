c7fe37dfac4f3c8815a240655e2562a7759d9016
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-03-22 19:41:09 +0100

    Initial commit

---
+++ src/main/java/pb/wi/cohp/ControlOfHealthParametersApplication.java
@@ -0,0 +1,13 @@
+package pb.wi.cohp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlOfHealthParametersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlOfHealthParametersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pb/wi/cohp/ControlOfHealthParametersApplicationTests.java
@@ -0,0 +1,13 @@
+package pb.wi.cohp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlOfHealthParametersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
