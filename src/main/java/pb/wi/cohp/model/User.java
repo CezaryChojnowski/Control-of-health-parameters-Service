73147c8e114b19cf0c8a86e4415ed039616ea3de
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-03-25 18:27:50 +0100

    Create role model and update user model

--- src/main/java/pb/wi/cohp/model/User.java
+++ src/main/java/pb/wi/cohp/model/User.java
@@ -8,16 +8,22 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    private String username;
 
     @Setter
     private String firstName;
@@ -40,6 +46,9 @@ public class User {
     @Setter
     private String token;
 
+    @Setter
+    private boolean enabled;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
     @JsonIgnore
     private List<Measure> measurement = new ArrayList<>();
@@ -59,5 +68,13 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
     @JsonIgnore
     private List<Range> ranges = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "id_user"),
+            inverseJoinColumns = @JoinColumn(name = "id_role")
+    )
+    private Set<Role> roles = new HashSet<>();
 
 }
