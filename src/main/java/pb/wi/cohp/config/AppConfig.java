14052b62742e39992b3de4370dad68617c58648a
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-03-25 18:33:28 +0100

    Implement and configure JWT

---
+++ src/main/java/pb/wi/cohp/config/AppConfig.java
@@ -0,0 +1,66 @@
+package pb.wi.cohp.config;
+
+import io.swagger.annotations.ApiKeyAuthDefinition;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+        LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
+        validatorFactoryBean.setValidationMessageSource(messageSource);
+        return validatorFactoryBean;
+    }
+
+    @Configuration
+    @EnableSwagger2
+    public static class SwaggerConfiguration {
+
+        @Bean
+        public LinkDiscoverers discoverers() {
+            List<LinkDiscoverer> plugins = new ArrayList<>();
+            plugins.add(new CollectionJsonLinkDiscoverer());
+            return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+        }
+
+        @Bean
+        public Docket jsonApi() {
+            return new Docket(SWAGGER_2)
+                    .securitySchemes(singletonList(new ApiKey("JWT", AUTHORIZATION, ApiKeyAuthDefinition.ApiKeyLocation.HEADER.name())))
+                    .securityContexts(singletonList(
+                            SecurityContext.builder()
+                                    .securityReferences(
+                                            singletonList(SecurityReference.builder()
+                                                    .reference("JWT")
+                                                    .scopes(new AuthorizationScope[0])
+                                                    .build()
+                                            )
+                                    )
+                                    .build())
+                    )
+                    .select()
+                    .build();
+        }
+    }
+
+}
