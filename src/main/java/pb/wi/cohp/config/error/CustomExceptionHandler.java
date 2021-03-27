68b1be69e4f241a9c3bfd0b7600ed36709e453a0
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-03-27 09:32:55 +0100

    Create custom response AuthenticationResponse and AccessDeniedResponse. Handle custom exception in CustomExceptionHandler

--- src/main/java/pb/wi/cohp/config/error/CustomExceptionHandler.java
+++ src/main/java/pb/wi/cohp/config/error/CustomExceptionHandler.java
@@ -4,14 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import pb.wi.cohp.config.error.exception.PasswordValidationFailedException;
-import pb.wi.cohp.config.error.exception.UserExistsException;
-import pb.wi.cohp.config.error.response.ConstraintViolationResponse;
-import pb.wi.cohp.config.error.response.PasswordValidationFailedResponse;
-import pb.wi.cohp.config.error.response.UserExistsResponse;
+import pb.wi.cohp.config.error.exception.*;
+import pb.wi.cohp.config.error.response.*;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
@@ -21,6 +20,7 @@ import java.util.List;
 @SuppressWarnings({"unchecked","rawtypes"})
 @ControllerAdvice
 @PropertySource("classpath:messages.properties")
+@PropertySource("classpath:PL.exception.messages.properties")
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Value("${registrationFailed}")
@@ -28,6 +28,37 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Value("${validationFailed}")
     private String validationFailed;
+
+    @Value("${loginFailed}")
+    private String loginFailed;
+
+    @Value("${accessDenied}")
+    private String accessDenied;
+
+    @Value("${authenticationException}")
+    private String authenticationException;
+
+    @Value("${userNotFound}")
+    private String userNotFound;
+
+    @Value("${accountActivationFailed}")
+    private String accountActivationFailed;
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<Object> handleAccessDeniedException(AccessDeniedException exception) {
+        List<String> details = new ArrayList<>();
+        details.add(exception.getLocalizedMessage());
+        AccessDeniedResponse error = new AccessDeniedResponse(accessDenied, details, HttpStatus.FORBIDDEN.value());
+        return new ResponseEntity(error, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<Object> handleAuthenticationException(AuthenticationException exception) {
+        List<String> details = new ArrayList<>();
+        details.add(authenticationException);
+        AuthenticationResponse error = new AuthenticationResponse(loginFailed, details, HttpStatus.UNAUTHORIZED.value());
+        return new ResponseEntity(error, HttpStatus.UNAUTHORIZED);
+    }
 
     @ExceptionHandler(UserExistsException.class)
     public ResponseEntity<Object> handleUsernameIsAlreadyTakenException(UserExistsException exception){
@@ -37,15 +68,37 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity(error, HttpStatus.CONFLICT);
     }
 
+    @ExceptionHandler(UserDoesNotHaveActiveAccountException.class)
+    public ResponseEntity<Object> handleUserDoesNotHaveActiveAccountException(UserDoesNotHaveActiveAccountException exception){
+        List<String> details = new ArrayList<>();
+        details.add(exception.getLocalizedMessage());
+        UserDoesNotHaveActiveAccountResponse error = new UserDoesNotHaveActiveAccountResponse(loginFailed, details, HttpStatus.CONFLICT.value());
+        return new ResponseEntity(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(IncorrectTokenException.class)
+    public ResponseEntity<Object> handleIncorrectTokenException(IncorrectTokenException exception){
+        List<String> details = new ArrayList<>();
+        details.add(exception.getLocalizedMessage());
+        IncorrectTokenResponse error = new IncorrectTokenResponse(accountActivationFailed, details, HttpStatus.CONFLICT.value());
+        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException exception) {
+        List<String> details = new ArrayList<>();
+        details.add(exception.getLocalizedMessage());
+        UserNotFoundResponse error = new UserNotFoundResponse(userNotFound, details, HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity(error, HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(PasswordValidationFailedException.class)
-    public ResponseEntity<Object> handleUsernameIsAlreadyTakenException(PasswordValidationFailedException exception){
+    public ResponseEntity<Object> handlePasswordValidationFailedException(PasswordValidationFailedException exception){
         List<String> details = new ArrayList<>();
         details.add(exception.getLocalizedMessage());
         PasswordValidationFailedResponse error = new PasswordValidationFailedResponse(validationFailed, details, HttpStatus.CONFLICT.value());
         return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
     }
-
-
 
     @ExceptionHandler({ ConstraintViolationException.class })
     public ResponseEntity handleConstraintViolation(
