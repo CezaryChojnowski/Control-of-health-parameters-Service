27b0a219f0a45adf5a1bc75a196557ff64208511
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-03-27 09:33:51 +0100

    Create method to generate token and activate account

--- src/main/java/pb/wi/cohp/domain/user/UserService.java
+++ src/main/java/pb/wi/cohp/domain/user/UserService.java
@@ -2,15 +2,20 @@ package pb.wi.cohp.domain.user;
 
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pb.wi.cohp.config.error.exception.IncorrectTokenException;
 import pb.wi.cohp.config.error.exception.PasswordValidationFailedException;
 import pb.wi.cohp.config.error.exception.UserExistsException;
+import pb.wi.cohp.config.error.exception.UserNotFoundException;
 import pb.wi.cohp.domain.role.ERole;
 import pb.wi.cohp.domain.role.Role;
 import pb.wi.cohp.domain.role.RoleRepository;
+import pb.wi.cohp.payload.response.MessageResponse;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -71,6 +76,7 @@ public class UserService {
                            String personalIdNumber,
                            String email,
                            String password,
+                           String token,
                            boolean active){
         checkIfExistsUserWithGivenUsername(username);
         checkIfExistsUserWithGivenEmail(email);
@@ -88,8 +94,50 @@ public class UserService {
                 .personalIdNumber(personalIdNumber)
                 .roles(roles)
                 .active(active)
+                .token(token)
                 .build();
         user.setPassword(encoder.encode(password));
         return userRepository.save(user);
+    }
+
+    public boolean checkIfUserHasActivatedAccount(String username){
+        return userRepository.findUserByUsername(username).get().isActive();
+    }
+
+    public String generateTokenToActiveAccount(){
+        int leftLimit = 48;
+        int rightLimit = 122;
+        int targetStringLength = 20;
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    public User getUserByEmail(String email){
+        return userRepository.findUserByEmail(email).get();
+    }
+
+    public void updateUser(User user){
+        userRepository.save(user);
+    }
+
+    public ResponseEntity<?> activateAccount(String email,
+                                   String token){
+        try{
+            User user = getUserByEmail(email);
+            if(token.equals(user.getToken())){
+                user.setActive(true);
+                user.setToken("");
+                userRepository.save(user);
+                return ResponseEntity.ok(new MessageResponse(env.getProperty("successfulActivateAccount")));
+            }
+        }catch (Exception ignored){
+            throw new UserNotFoundException(env.getProperty("emailNotFound"));
+        }
+        throw new IncorrectTokenException(env.getProperty("badToken"));
     }
 }
