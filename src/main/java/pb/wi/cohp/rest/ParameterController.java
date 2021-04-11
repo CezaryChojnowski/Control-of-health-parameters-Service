1f9178c6fb94cda02858ae697560234ac37ca612
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-04-12 00:57:27 +0200

    Create ParameterController with remove and edit parameter HTTP methods

---
+++ src/main/java/pb/wi/cohp/rest/ParameterController.java
@@ -0,0 +1,31 @@
+package pb.wi.cohp.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import pb.wi.cohp.domain.parameter.Parameter;
+import pb.wi.cohp.domain.parameter.ParameterService;
+
+@RestController
+@RequestMapping("/parameters")
+public class ParameterController {
+
+    final ParameterService parameterService;
+
+    public ParameterController(ParameterService parameterService) {
+        this.parameterService = parameterService;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("/{parameterId}")
+    public void removeParameter(@PathVariable Long parameterId){
+        parameterService.deleteParameter(parameterId);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping
+    public ResponseEntity<?> editParameter(@RequestBody Parameter parameter){
+        return ResponseEntity.ok(parameterService.editParameter(parameter));
+    }
+
+}
