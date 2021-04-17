bfed66af758ef9ce85b48d2146f1597f5959259a
CezaryChojnowski <cezary.mateusz.chojnowski@gmail.com>
2021-04-17 15:29:07 +0200

    Create ParameterDTO and TestDTO classes

---
+++ src/main/java/pb/wi/cohp/domain/parameter/ParameterDTO.java
@@ -0,0 +1,13 @@
+package pb.wi.cohp.domain.parameter;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Setter
+public class ParameterDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/pb/wi/cohp/domain/test/TestDTO.java
@@ -0,0 +1,18 @@
+package pb.wi.cohp.domain.test;
+
+
+import lombok.*;
+import pb.wi.cohp.domain.parameter.Parameter;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Setter
+public class TestDTO {
+    private Long id;
+    private String name;
+    List<Parameter> parameters;
+}
